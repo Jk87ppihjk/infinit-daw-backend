ea7576439927af13bf5b14b3e5db3ac9cfecb539
Jk87ppihjk <tutano172@gmail.com>
2025-06-22 13:21:42 -0300

    Delete daww directory

--- daww/js/17_compressorUI.js
+++
@@ -1,2 +0,0 @@
-// app/js/17_compressorUI.js
-// Conteúdo movido para app/effects/compressor/compressor.js
--- daww/js/18_parametricEqUI.js
+++
@@ -1,2 +0,0 @@
-// app/js/18_parametricEqUI.js
-// Conteúdo movido para app/effects/parametric_eq/parametric_eq.js
